895fbcbddd84ef78d4ef03bc2764807b93d9ef99
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-03-09 08:28:22 +0200

    fix overflow problrm

--- lib/core/utils/app_theme.dart
+++ lib/core/utils/app_theme.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
-  return ThemeData(brightness: Brightness.dark);
+  return ThemeData(
+    brightness: Brightness.dark,
+    appBarTheme:const AppBarTheme(
+      centerTitle: true,
+    ),
+  );
 }
--- lib/features/home/presentation/view/home.dart
+++ lib/features/home/presentation/view/home.dart
@@ -1,10 +1,87 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Slash.",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: GridView.builder(
+        // itemCount: 4,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: .6, mainAxisSpacing: 10.h),
+        itemBuilder: (context, index) => const CustomProductCard(),
+      ),
+    );
+  }
+}
+
+class CustomProductCard extends StatelessWidget {
+  const CustomProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12.w),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          16,
+        ),
+        border: Border.all(color: Colors.white),
+      ),
+      child: Column(
+        children: [
+          Image.network(
+            'https://firebasestorage.googleapis.com/v0/b/slash-d7546.appspot.com/o/categories%2Fcanvas.png?alt=media&token=348aae1e-d4df-43f5-9d48-c50dbf5abb26',
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                  child: const Text(
+                      'Brand NAme and just to show it fd fdff fdfd frgr tg gf  fferfrfr grgr ed r grgrg  ')),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: CircleAvatar(
+                  radius: 15,
+                  backgroundColor: Colors.green,
+                  child: Image.network(
+                    'https://firebasestorage.googleapis.com/v0/b/slash-d7546.appspot.com/o/categories%2Fcanvas.png?alt=media&token=348aae1e-d4df-43f5-9d48-c50dbf5abb26',
+                  ),
+                ),
+              )
+            ],
+          ),
+          const Spacer(),
+          Row(
+            children: [
+              const Text('EGP 365 '),
+              const Spacer(),
+              const Icon(Icons.favorite_border_outlined),
+              Gap(10.w),
+              const Icon(
+                Icons.shopping_cart,
+                color: Colors.grey,
+              )
+            ],
+          ),
+          Gap(20.h),
+        ],
+      ),
+    );
   }
 }
