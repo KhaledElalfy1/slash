f8dd3479ccc5e80c81cc6ce7b47267bc2aa577c3
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-03-09 08:44:17 +0200

    add shimmer for loading

--- lib/core/widgets/custom_network_image.dart
+++ lib/core/widgets/custom_network_image.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:slash/core/widgets/Loading_image.dart';
 
 class CustomNetworkImage extends StatelessWidget {
   const CustomNetworkImage({super.key, required this.imageUrl});
@@ -9,7 +10,7 @@ class CustomNetworkImage extends StatelessWidget {
     return CachedNetworkImage(
       imageUrl: imageUrl,
       placeholder: (context, url) =>
-          const Center(child: CircularProgressIndicator()),
+          const Center(child: CustomLoadingImage()),
       errorWidget: (context, url, error) => const Icon(Icons.error),
     );
   }
